13883d1e279c8f31b9311629862d152a094fbcc7
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-11-16 19:04:50 +0000

    added prop 4 and 10
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

---
+++ monitoring/prop10.py
@@ -0,0 +1,33 @@
+# property to verify
+"""
+H((P[5:] True) IMPLIES P[:5] people_following_published)"""
+
+PROPERTY = r"historically(once[5:]]{t} -> once[:5]{people_following_published})"
+
+# predicates used in the property (initialization for time 0)
+
+# in here we can add all the predicates we are interested in.. Of course, we also need to define how to translate Json messages to predicates.
+
+# function to abstract a dictionary (obtained from Json message) into a list of predicates
+
+predicates = dict(
+
+    people_following_published = False,
+
+    time = 0
+
+)
+
+# in here we can add all the predicates we are interested in.. Of course, we also need to define how to translate Json messages to predicates.
+
+# function to abstract a dictionary (obtained from Json message) into a list of predicates
+
+def abstract_message(message):
+    predicates['time'] = message['time']
+
+    if message['topic'] == "clock":
+        predicates['people_following_published'] = False
+    elif message['topic'] == "PeopleDetectorFilterComponent/is_followed":
+        predicates['people_following_published'] = True
+
+    return predicates
