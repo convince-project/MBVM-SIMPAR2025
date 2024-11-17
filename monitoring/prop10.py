532158005b0d02ac10243b04fe74101a0961e293
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-11-17 12:25:29 +0000

    updated monitoring files
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- monitoring/prop10.py
+++ monitoring/prop10.py
@@ -2,7 +2,7 @@
 """
 H((P[5:] True) IMPLIES P[:5] people_following_published)"""
 
-PROPERTY = r"historically(once[3:]{t} -> once[:10]{people_following_published})"
+PROPERTY = r"historically(once[2:]{t} -> once[:10]{people_following_published})"
 
 # predicates used in the property (initialization for time 0)
 
