3bcae6fc8ce270b20024cfd1720a8eab9da0a50f
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-11-22 13:47:58 +0000

    added prop 3
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- monitoring/prop3.py
+++ monitoring/prop3.py
@@ -1,37 +1,26 @@
+# property to verify
 """
-H((alarm => P battery_level < 30%) AND - ( -alarm S[5 : ] battery_level  < 30%))"""
+H((P[5:] True) IMPLIES P[:5] battery_published)"""
 
-PROPERTY = r"historically(({alarm} -> once{low_battery}) and not( not {alarm} since[5:] {low_battery}))"
+PROPERTY = r"historically(once[5:]{t} -> once[:5]{battery_published})"
 
-# predicates used in the property (initialization for time 0)
-
-# in here we can add all the predicates we are interested in.. Of course, we also need to define how to translate Json messages to predicates.
-
-# function to abstract a dictionary (obtained from Json message) into a list of predicates
 
 predicates = dict(
 
-    time = 0,
+    battery_published = False,
 
-    alarm = False,
+    t = True,
 
-    low_battery = False,
+    time = 0
 
 )
 
-# in here we can add all the predicates we are interested in.. Of course, we also need to define how to translate Json messages to predicates.
-
-# function to abstract a dictionary (obtained from Json message) into a list of predicates
-
 def abstract_message(message):
-
-    if message['time'] <= predicates['time']:
-        predicates['time'] += 0.0000001
-    else:
-        predicates['time'] = message['time']
-
-    predicates['alarm'] = message['service'] == "AlarmSkill/tick" if 'service' in message else predicates['alarm']
-
-    predicates['low_battery'] = message['percentage'] < 31 if 'percentage' in message else predicates['low_battery']
-
+    predicates['time'] = message['time']
+    print("message", message)
+    if message['topic'] == "clock":
+        predicates['battery_published'] = False
+    elif message['topic'] == "battery_level":
+        predicates['battery_published'] = True
+    print("predicates", predicates)
     return predicates
