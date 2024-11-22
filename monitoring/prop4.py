dd222ea5b49dd97b5c406baac5695e10a18a88f9
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2024-11-22 13:50:17 +0000

    added prop4
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- monitoring/prop4.py
+++ monitoring/prop4.py
@@ -1,8 +1,8 @@
 # property to verify
-"""
-H((P[5:] True) IMPLIES P[:5] battery_published)"""
 
-PROPERTY = r"historically(once[5:]{t} -> once[:5]{battery_published})"
+'''   H(POI_1_selected => P -POI_1_completed) AND - (-POI_1_selected S[2 : ] -POI_1_completed)
+'''
+PROPERTY = r"historically(({poi1_selected} -> once( not {poi1_completed})) and not( not {poi1_selected} since [5:] not {poi1_completed}))"
 
 # predicates used in the property (initialization for time 0)
 
@@ -12,11 +12,11 @@ PROPERTY = r"historically(once[5:]{t} -> once[:5]{battery_published})"
 
 predicates = dict(
 
-    battery_published = False,
+    poi1_selected = False,
 
-    t = True,
+    poi1_completed = False,
 
-    time = 0
+    time = 0,
 
 )
 
@@ -25,11 +25,32 @@ predicates = dict(
 # function to abstract a dictionary (obtained from Json message) into a list of predicates
 
 def abstract_message(message):
-    predicates['time'] = message['time']
 
-    if message['topic'] == "clock":
-        predicates['battery_published'] = False
-    elif message['topic'] == "battery_level":
-        predicates['battery_published'] = True
+    if message['time'] <= predicates['time']:
+        predicates['time'] += 0.0000001
+    else:
+        predicates['time'] = message['time']
+
+    print("message", message)
+    print("predicates", predicates)
+
+    # int8 SKILL_SUCCESS=0
+    # int8 SKILL_FAILURE=1
+    # int8 SKILL_RUNNING=2
+    if "service" in message and "response" in message:
+        if message['service'] == "IsPoiDone1Skill/tick" and message['response']['status'] == 0:
+            predicates['poi1_completed'] = True
+        elif message['service'] == "IsPoiDone1Skill/tick" and message['response']['status'] != 0:
+            predicates['poi1_completed'] = False
+
+        if message['service'] == "SetPoi1Skill/tick" and message['response']['status'] == 0:
+            predicates['poi1_selected'] = True
+        elif message['service'] == "SetPoi1Skill/tick" and message['response']['status'] != 0:
+            predicates['poi1_selected'] = False
+
+    # predicates['service'] = True if 'service' in message else False
+
+    # predicates['low_percentage'] = True if 'percentage' in message and message['percentage'] < 30 else False
+
 
     return predicates
